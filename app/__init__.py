e104bc55218900f4f78c0d29fe39778491f0fd7c
nickname <v.yahnavets@gmail.com>
2020-05-19 12:44:10 +0300

    base dannix sdelali

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 App = Flask(__name__)
 App.config.from_object(Configuration)
 db = SQLAlchemy(App)
 migrate = Migrate(App, db)
+login = LoginManager(App)
 
-from app import routes
+from app import routes, models
