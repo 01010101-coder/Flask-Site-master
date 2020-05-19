1ebb2186aac165126f71c8c63133d079665ecf7c
nickname <v.yahnavets@gmail.com>
2020-05-19 11:23:09 +0300

    Initial commit

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+basedir = os.path.abspath(os.dirname(__file__))
 
 class Configuration:
     SECRET_KEY = os.environ.get('SECRET_KEY') or "E1FE22BC394EE1FEADD233BB8C34C"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
