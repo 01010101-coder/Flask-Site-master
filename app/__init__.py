8986223230dc8ffdb4a83162b96201035bf49430
nickname <v.yahnavets@gmail.com>
2020-05-19 11:27:59 +0300

    base dannix

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from config import Configuration
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 App = Flask(__name__)
 App.config.from_object(Configuration)
-
+db = SQLAlchemy(App, db)
 
 from app import routes
